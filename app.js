d3ca1b192074c2626f07e17f5d2f67acde948698
Eder <ea0478405@gmail.com>
2024-10-28 13:14:23 -0600

    Corrección en app.js y configuración de vercel.json

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('App escuchando en http://localhost:${port}');
+    console.log(`App escuchando en http://localhost:${port}`);
 });
